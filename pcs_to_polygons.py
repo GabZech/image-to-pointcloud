d304b6031da34a257eb87dc47d689b93d97791af
Nassim Zoueini <78618443+nassimzoueini@users.noreply.github.com>
2023-04-13 03:58:52 +0200

    new dictionary of roof panes, code to check point cloud size

--- pcs_to_polygons.py
+++ pcs_to_polygons.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 #Always work on 50526389
 #Test on this 50520918
 pointsList=[]
-with open('data/run2/processed/pcs/clean_saddle_roofs/108491.json') as f:
+with open('data/run2/processed/pcs/saddle_roofs/103056683.json') as f:
     for jsonObj in f:
         pt_coord=json.loads(jsonObj)
         pointsList.append(pt_coord['lidar'])
@@ -42,7 +42,7 @@ pcd.points=o3d.utility.Vector3dVector(points)
 #      pcd=outlier_cloud
 #
 #Reference https://towardsdatascience.com/how-to-automate-3d-point-cloud-segmentation-and-clustering-with-python-343c9039e4f5
-max_plane_idx=5  
+max_plane_idx=2
 
 segment_models={}
 segments={}
@@ -82,7 +82,73 @@ print(f"point cloud has {max_label + 1} clusters")
 colors = plt.get_cmap("tab10")(labels / (max_label if max_label > 0 else 1))
 colors[labels < 0] = 0
 rest.colors = o3d.utility.Vector3dVector(colors[:, :3])
-o3d.visualization.draw([segments[i] for i in range(max_plane_idx)]+[rest])
+o3d.visualization.draw([segments[i] for i in range(max_plane_idx)])#+[rest])
 #print(roof)
 
 
+#%%
+import numpy as np
+import laspy as laspy
+import pyvista as pv
+import json
+import open3d as o3d
+import matplotlib.pyplot as plt
+import os
+
+pointsList=[]
+with open('data/run2/processed/pcs/saddle_roofs/103056683.json') as f:
+    name=103056683
+    for jsonObj in f:
+        pt_coord=json.loads(jsonObj)
+        pointsList.append(pt_coord['lidar'])
+
+# print(pointsList)
+points=np.asarray(pointsList, dtype=np.float32)
+points=points[0,:,:]
+pcd = o3d.geometry.PointCloud()
+pcd.points=o3d.utility.Vector3dVector(points)
+
+max_plane_idx=2
+
+segment_models={}
+segments={}
+roof={}
+
+rest=pcd
+d_threshold=100
+
+for i in range(max_plane_idx):
+    colors = plt.get_cmap("tab20")(i)
+    segment_models[i], inliers = rest.segment_plane(distance_threshold=15,ransac_n=3,num_iterations=1000)
+    [a, b, c, d] = segment_models[i]
+    print("Displaying pointcloud with planar points in",i+1, "th segment",f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
+    angle=np.fix(np.arccos(c)*180/np.pi%90)
+    print("Inclination=",angle, "°")
+    segments[i]=rest.select_by_index(inliers)
+    labels = np.array(segments[i].cluster_dbscan(eps=d_threshold*100, min_points=10))
+    candidates=[len(np.where(labels==j)[0]) for j in np.unique(labels)]
+    best_candidate=int(np.unique(labels)[np.where(candidates== np.max(candidates))[0]])
+    segments[i].paint_uniform_color(list(colors[:3]))
+    rest = rest.select_by_index(inliers, invert=True) + segments[i].select_by_index(list(np.where(labels!=best_candidate)[0]))
+    segments[i]=segments[i].select_by_index(list(np.where(labels== best_candidate)[0]))
+    print("pass",i,"/",max_plane_idx,"done.")
+
+    
+    key= "roof_pane_" + str(i)
+    id= name
+    pts=segments[i]
+    vector=[a,b,c,d]
+    inclination=angle
+    roof[key]={"id":id, "pts":pts, "vector":vector, "inclination":inclination}	
+
+# roof.get('inclination')
+
+labels = np.array(rest.cluster_dbscan(eps=0.05, min_points=5))
+max_label = labels.max()
+print(f"point cloud has {max_label + 1} clusters")
+colors = plt.get_cmap("tab10")(labels / (max_label if max_label > 0 else 1))
+colors[labels < 0] = 0
+rest.colors = o3d.utility.Vector3dVector(colors[:, :3])
+o3d.visualization.draw([segments[i] for i in range(max_plane_idx)])#+[rest])
+
+# %%
