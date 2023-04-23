fa2fdc1e81b323871e7c0e04d690083f994256aa
Gabriel da Silva Zech <gabrielszech@gmail.com>
2023-04-23 20:18:30 +0200

    ✨ add code to match img and pc tiles

--- src/select_tiles.py
+++ src/select_tiles.py
@@ -1,46 +1,10 @@
-# #%%
-# import pandas as pd
-
-# n_samples = 300
-
-# # IMAGES
-
-# meta_img = pd.read_csv("data/raw/images/dop_nw.csv")
-
-# # randomly select n tiles from the meta_img
-# meta_img_sample = meta_img.sample(n_samples, random_state=41)
-
-# meta_img_sample.to_csv("data/tiles_sample_img.csv", index=False)
-
-# # %%
-
-# # POINTCLOUDS
-
-# from functions_process import extract_coords_tilename
-
-# tiles_img = meta_img_sample["Kachelname"].tolist()
-# meta_pc = pd.read_csv("data/raw/pcs/3dm_nw.csv")
-# tiles_pc_all = meta_pc["Kachelname"].tolist()
-
-# meta_pc_sample = pd.DataFrame()
-# coords = list(map(extract_coords_tilename, tiles_img))
-# for i in range(len(coords)):
-#     x, y = coords[i][0], coords[i][1]
-#     tile = meta_pc[meta_pc["Kachelname"].str.contains(f"{x}_{y}")]
-#     meta_pc_sample = pd.concat([meta_pc_sample, tile])
-#     # for tile in tiles_pc_all:
-#     #     if f"{x}_{y}" in tile:
-#     #         tiles_pc.append(tile)
-
-# meta_pc_sample.to_csv("data/processed/tiles_sample_pcs.csv", index=False)
-
-
 # %%
-
 import pandas as pd
 
 from functions_download import download_metadata
 from functions_process import extract_coords_tilename
+
+# %% Download metadata for tiles and find matching tiles
 
 download_metadata(raw_data_folder="data/",
     metadata_filename="dop_nw.csv",
@@ -54,8 +18,8 @@ download_metadata(raw_data_folder="data/",
     skiprows=6,
     rewrite_download=True)
 
-meta_img = pd.read_csv("data/raw/images/dop_nw.csv")
-meta_pc = pd.read_csv("data/raw/pcs/3dm_nw.csv")
+meta_img = pd.read_csv("data/dop_nw.csv")
+meta_pc = pd.read_csv("data/3dm_nw.csv")
 
 meta_img["coords"] = meta_img["Kachelname"].apply(extract_coords_tilename)
 meta_pc["coords"] = meta_pc["Kachelname"].apply(extract_coords_tilename)
@@ -74,29 +38,60 @@ merged = pd.merge(meta_img_s, meta_pc_s, on="coords", how="inner")
 merged["Aktualitaet_img"] = pd.to_datetime(merged["Aktualitaet_img"], format="%Y-%m-%d")
 merged["Aktualitaet_pc"] = pd.to_datetime(merged["Aktualitaet_pc"], format="%Y-%m-%d")
 
-merged["diff"] = abs(merged["Aktualitaet_img"] - merged["Aktualitaet_pc"])
+merged["diff"] = abs((merged["Aktualitaet_img"] - merged["Aktualitaet_pc"]).dt.days)
 
 # sort by difference
 merged = merged.sort_values(by="diff", ascending=True).reset_index(drop=True)
 
 merged.to_csv("data/tiles_merged.csv", index=False)
 
-#%%
+#%% plot histogram of time differences
 import matplotlib.pyplot as plt
 
 plt.figure(figsize=(14,5))
 avg = merged["diff"].mean()
 print(f"Average difference: {avg} days")
-max_value = max(list(merged["diff"].dt.days))
+max_value = max(list(merged["diff"]))
 max_diff = (max_value // 30) + 1
 #plot = merged["diff"].dt.days.plot.hist(bins=range(max_diff))
 xtick = [i*30 for i in range(max_diff+1)]
-plt.hist(merged["diff"].dt.days, bins=xtick)
+plt.hist(merged["diff"], bins=xtick)
 plt.xticks(xtick, rotation=75)
 plt.xlabel("Days")
 plt.ylabel("Tile count")
-plt.title(f"Difference in days between collection date of image and pointcloud tiles\n(n={len(merged)}, mean={avg.days} days)");
-plt.axvline(x=merged["diff"].dt.days.mean(), color="red", label="mean")
+plt.title(f"Difference in days between collection date of image and pointcloud tiles\n(n={len(merged)}, mean={int(avg)} days)");
+plt.axvline(x=avg, color="red", label="mean")
 plt.rcParams.update({'font.size': 12})
 
-# %%
+# %% check difference in dates across tiles
+import os
+import pandas as pd
+import geopandas as gpd
+
+data_dir = "data/all/processed"
+
+gdf = gpd.GeoDataFrame()
+for f in os.listdir(data_dir):
+    if f.startswith("buildings_metadata"):
+        print(f"Found {f}")
+        gdf_ = gpd.read_file(f"{data_dir}/{f}")
+        gdf = pd.concat([gdf, gdf_])
+
+sub_dirs = next(os.walk(f"{data_dir}/images"))[1]
+
+images = []
+for sub_dir in sub_dirs:
+    if sub_dir != "check":
+        # list all files in sub_dir
+        [images.append(f[:-4]) for f in os.listdir(f"{data_dir}/images/{sub_dir}")]
+
+# filter gdf to only include found images
+gdf = gdf[gdf["building_id"].isin(images)]
+
+# add time information
+all_tiles = pd.read_csv("data/tiles_merged.csv")
+gdf = pd.merge(gdf, all_tiles, left_on="kachelname", right_on="Kachelname_img", how="left")
+
+above_2years = gdf[gdf["diff"] > 365*2]
+
+plot = gdf["diff"].plot.hist(bins=100)
